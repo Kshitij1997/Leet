39184b29b3670a25830e055ed4f88f04fff84e94
Kshitij1997 <31150336+Kshitij1997@users.noreply.github.com>
2024-09-10 15:00:42 -0400

    Nth Fibonacci number
    Attempt for generating nth fibonacci number using recursion.
    
    Logic for induction is given in comments.
    
    It goes like below:
    Try describing the function using f(n-1), f(n-2).
    Determine the base case
    Make sure you handle boundary condition.

--- src/main/java/Temp/Recursive.java
+++ src/main/java/Temp/Recursive.java
@@ -2,7 +2,9 @@ package Temp;
 
 public class Recursive {
     public static void main(String []args){
+
         System.out.println(recursive_Factorial(5));
+        System.out.println(recursive_Fibo(19));
     }
 
     // fn = n * fn-1
@@ -14,6 +16,22 @@ public class Recursive {
         }
 
         return  n * recursive_Factorial(n-1);
+    }
+
+    // return nth fibonacci number
+    // fn = f(n-1) + f(n-2)
+    // n = 1 , 0
+    // n = 2  = 1
+    public static int recursive_Fibo(int n){
+        if(n <= 0){
+            return -1;
+        }
+        if(n == 1){
+            return 0;
+        } else if (n == 2) {
+            return 1;
+        }
+        return recursive_Fibo(n-1) + recursive_Fibo(n-2);
     }
 }
 
